209ee3cfd6b51a758589fba97f3405da27b3d0d0
canaleija <robanac@gmail.com>
2024-09-11 10:27:22 -0600

    SE agrega codigo de prueba de commit pasado

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import introduccion.Credencial;
 import introduccion.Estudiante;
+import introduccion.Fecha;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -8,8 +10,11 @@ public class App {
         //System.out.println("Hola hola");
 
         // declaración
-        Estudiante juan = new Estudiante();
-        
+        Fecha f1 = new Fecha(12, 2, 2024);
+        Fecha f2 = new Fecha(30, 12, 2030);
+        Credencial aux = new Credencial("Juan Alberto", "Sistemas", 2020670089,f1,f2, "UPIIZ");
+        Estudiante juan = new Estudiante(aux);
+
         
         System.out.println();
     }
